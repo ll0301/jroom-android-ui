97a4860f713ace4615cd54b59fc9b6e4326a7225
hun <wanted0301@gmail.com>
2020-10-19 01:09:34 +0900

    add emotional face view

---
+++ app/src/main/java/com/example/jroom_anc_ui/EmotionalActivity.kt
@@ -0,0 +1,20 @@
+package com.example.jroom_anc_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_emotional.*
+
+class EmotionalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emotional)
+    
+        happyButton.setOnClickListener {
+            emotionalFaceView.happinessState = EmotionalFaceView.HAPPY
+        }
+    
+        sadButton.setOnClickListener {
+            emotionalFaceView.happinessState = EmotionalFaceView.SAD
+        }
+    }
+}
