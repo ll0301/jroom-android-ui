74481b40c34e202d6d12c89d5fc66ca8914208f4
hun <wanted0301@gmail.com>
2020-10-26 14:18:24 +0900

    create branch of intro swipe

--- app/src/main/java/com/example/jroomui/data/model/Category.kt
+++
@@ -1,4 +0,0 @@
-package com.example.jroomui.data.model
-
-class Category {
-}
--- app/src/main/java/com/example/jroomui/main/MainActivity.kt
+++ app/src/main/java/com/example/jroomui/main/MainActivity.kt
@@ -6,8 +6,6 @@ import com.example.jroomui.R
 
 class MainActivity : AppCompatActivity() {
     
-    private lateinit var viewModel: MainViewModel
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
